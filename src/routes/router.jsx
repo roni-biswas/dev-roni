9bbb3f685c5442e045ce1cc3cb2fc755ad34b1f6
Roni Biswas <ronibiswas068@gmail.com>
2025-06-29 21:21:11 +0600

    added initial router setup and layout integrate

---
+++ src/routes/router.jsx
@@ -0,0 +1,18 @@
+import { createBrowserRouter } from "react-router";
+import MainLayout from "../layout/MainLayout";
+import Home from "../pages/Home/Home";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: MainLayout,
+    children: [
+      {
+        index: true,
+        Component: Home,
+      },
+    ],
+  },
+]);
+
+export default router;
